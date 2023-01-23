85665c359e392b1204e722c9c14b04e556af2e3b
hammadmajid <hm.hammadmajid@gmail.com>
2023-01-23 15:35:43 +0500

    Update: test hero content and functionality

--- cypress/e2e/hero.cy.ts
+++ cypress/e2e/hero.cy.ts
@@ -1,12 +1,36 @@
-describe('Test Hero section on homepage', () => {
+describe('Has correct content', () => {
+    before(() => {
+        cy.visit('/')
+    })
+
+    it('Displays the correct title', () => {
+        cy.get('.hero > .hero-body > .title')
+            .contains('Astro');
+    })
+
+    it('Displays the correct subtitle', () => {
+        cy.get('.hero > .hero-body > .subtitle')
+            .contains('Production ready, modern and high performance website template built with Astro.js and Bulma.io')
+    })
+})
+
+describe('Buttons work as expected', () => {
     beforeEach(() => {
         cy.visit('/')
     })
 
-    it('Displays the correct heading', () => {
-        cy.get('h1')
-            .contains('Astro');
+    it('Tests primary button', () => {
+        cy.get('.hero > .hero-body > .buttons > .is-secondary')
+            .contains('Get Template').click()
+        cy.url().should('include', '/#')
+
+    })
+
+    it('Tests secondary button', () => {
+        cy.get('.hero > .hero-body > .buttons > .is-light')
+            .contains('View code').click()
+        cy.url().should('include', '/#')
     })
 })
 
-export{}
+export { }
