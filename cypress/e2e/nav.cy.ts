50c690060b0598d0320b4ad7a146310ac4c8de3e
hammadmajid <hm.hammadmajid@gmail.com>
2023-02-01 21:12:53 +0500

    Test: add tests for refactored navbar

--- cypress/e2e/nav.cy.ts
+++ cypress/e2e/nav.cy.ts
@@ -10,34 +10,34 @@ describe('Tests NavBar functionality', () => {
 
         cy.get('#pricing-link')
             .contains('Pricing')
-            .should('have.attr', 'href').and('include', '/')
+            .should('have.attr', 'href').and('include', '/pricing')
 
         cy.get('#contact-link')
             .contains('Contact')
-            .should('have.attr', 'href').and('include', '/')
+            .should('have.attr', 'href').and('include', '/contact')
 
         cy.get('#about-link')
             .contains('About')
-            .should('have.attr', 'href').and('include', '/')
+            .should('have.attr', 'href').and('include', '/about')
 
         cy.get('#more-link')
             .contains('More')
 
         cy.get('#license-link')
             .contains('License')
-            .should('have.attr', 'href').and('include', '/')
+            .should('have.attr', 'href').and('eq', 'https://creativecommons.org/publicdomain/zero/1.0/')
 
-        cy.get('#issues-link')
-            .contains('Issues')
-            .should('have.attr', 'href').and('include', '/')
+        cy.get('#project-link')
+            .contains('Roadmap')
+            .should('have.attr', 'href').and('eq', 'https://github.com/users/hammadmajid/projects/10')
 
-        cy.get('#pr-link')
-            .contains('Pull requests')
-            .should('have.attr', 'href').and('include', '/')
+        cy.get('#sponsor-link')
+            .contains('Sponsor')
+            .should('have.attr', 'href').and('eq', 'https://patreon.com/hammadmajid')
 
-        cy.get('#github-link')
-            .contains('GitHub')
-            .should('have.attr', 'href').and('include', '/')
+        cy.get('#prototype-link')
+            .contains('Prototype')
+            .should('have.attr', 'href').and('eq', 'https://www.figma.com/community/file/1197507698947430863')
     })
 })
 
